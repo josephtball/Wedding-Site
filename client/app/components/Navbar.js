b9dbf8b7ba4d5b54cd61f275500c89ede1b0f857
Joseph Ball <jball321@live.com>
2018-01-28 21:31:53 -0500

    Added method to toggle hamburger icon.

--- client/app/components/Navbar.js
+++ client/app/components/Navbar.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Navbar = (props) => {
-    return (
-        <nav>
-            <div className="menu-toggle">
-                <div className="hamburger-top"></div>
-                <div className="hamburger-middle"></div>
-                <div className="hamburger-bottom"></div>
-            </div>
-            <div className="site-nav">
-                <ul>
-                    <li><a className="nav-link scroll" href="#home">home</a></li>
-                    <li><a className="nav-link scroll" href="#couple">the couple</a></li>
-                    <li><a className="nav-link scroll" href="#wedding-party">wedding party</a></li>
-                    <li><a className="nav-link scroll" href="#schedule">schedule</a></li>
-                    <li><a className="nav-link scroll" href="#gallery">gallery</a></li>
-                </ul>
-            </div>
-        </nav>
-    );
+export default class Navbar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { openCloseMenu: false };
+
+        this.toggleClass = this.toggleClass.bind(this);
+    }
+
+    render() {
+        return (
+            <nav>
+                <div onClick={this.toggleClass} className={`menu-toggle${this.state.openCloseMenu ? ' open': ''}`}>
+                    <div className="hamburger-top"></div>
+                    <div className="hamburger-middle"></div>
+                    <div className="hamburger-bottom"></div>
+                </div>
+                <div className="site-nav">
+                    <ul>
+                        <li><a className="nav-link scroll" href="#home">home</a></li>
+                        <li><a className="nav-link scroll" href="#couple">the couple</a></li>
+                        <li><a className="nav-link scroll" href="#wedding-party">wedding party</a></li>
+                        <li><a className="nav-link scroll" href="#schedule">schedule</a></li>
+                        <li><a className="nav-link scroll" href="#gallery">gallery</a></li>
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+
+    toggleClass() {
+        let currentState = this.state.openCloseMenu;
+        this.setState({ openCloseMenu: !currentState });
+    }
 }
-
-export default Navbar;
