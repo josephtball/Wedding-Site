53393eac98a92a48e1c67e99cafee82653d0634e
Joseph Ball <jball321@live.com>
2018-03-01 21:48:26 -0500

    Setup React Router.

---
+++ client/app/components/Schedule.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export default class Main extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <main>
+                <section className="content">
+                    <h2>Schedule</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu tristique metus, at maximus nisl. Aliquam in neque rhoncus, suscipit sem eget, sodales neque. Suspendisse sit amet ipsum leo. Phasellus a nisl viverra, molestie neque et, aliquam mauris. Nullam vulputate augue a massa accumsan blandit.</p>
+                    <button className="rsvp" disabled>RSVP</button>
+                </section>
+            </main>
+        )
+    }
+}
