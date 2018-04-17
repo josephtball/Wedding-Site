2920bbb18e2c3c57e1ff27790bfca10442172388
Joseph Ball <jball321@live.com>
2018-04-16 23:00:56 -0400

    Added api route to get eventList data.

---
+++ server/data/eventList.js
@@ -0,0 +1,49 @@
+var eventList = [
+    {
+        'title': 'Event One',
+        'date': '1/1/2018',
+        'time': '1:00PM',
+        'imgPath': '../static/assets/images/event.jpg',
+        'location': 'The Event Holding Place',
+        'cords': [ 11, 11 ],
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu tristique metus, at maximus nisl. Aliquam in neque rhoncus, suscipit sem eget, sodales neque. Suspendisse sit amet ipsum leo. Phasellus a nisl viverra, molestie neque et, aliquam mauris. Nullam vulputate augue a massa accumsan blandit.'
+    },
+    {
+        'title': 'Event Two',
+        'date': '2/2/2018',
+        'time': '2:00PM',
+        'imgPath': '../static/assets/images/event.jpg',
+        'location': 'The Event Holding Place',
+        'cords': [ 11, 11 ],
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu tristique metus, at maximus nisl. Aliquam in neque rhoncus, suscipit sem eget, sodales neque. Suspendisse sit amet ipsum leo. Phasellus a nisl viverra, molestie neque et, aliquam mauris. Nullam vulputate augue a massa accumsan blandit.'
+    },
+    {
+        'title': 'Event Three',
+        'date': '3/3/2018',
+        'time': '3:00PM',
+        'imgPath': '../static/assets/images/event.jpg',
+        'location': 'The Event Holding Place',
+        'cords': [ 11, 11 ],
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu tristique metus, at maximus nisl. Aliquam in neque rhoncus, suscipit sem eget, sodales neque. Suspendisse sit amet ipsum leo. Phasellus a nisl viverra, molestie neque et, aliquam mauris. Nullam vulputate augue a massa accumsan blandit.'
+    },
+    {
+        'title': 'Event Four',
+        'date': '4/4/2018',
+        'time': '4:00PM',
+        'imgPath': '../static/assets/images/event.jpg',
+        'location': 'The Event Holding Place',
+        'cords': [ 11, 11 ],
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu tristique metus, at maximus nisl. Aliquam in neque rhoncus, suscipit sem eget, sodales neque. Suspendisse sit amet ipsum leo. Phasellus a nisl viverra, molestie neque et, aliquam mauris. Nullam vulputate augue a massa accumsan blandit.'
+    },
+    {
+        'title': 'Event Five',
+        'date': '5/5/2018',
+        'time': '5:00PM',
+        'imgPath': '../static/assets/images/event.jpg',
+        'location': 'The Event Holding Place',
+        'cords': [ 11, 11 ],
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu tristique metus, at maximus nisl. Aliquam in neque rhoncus, suscipit sem eget, sodales neque. Suspendisse sit amet ipsum leo. Phasellus a nisl viverra, molestie neque et, aliquam mauris. Nullam vulputate augue a massa accumsan blandit.'
+    },
+];
+
+module.exports = eventList;
