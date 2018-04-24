4a1487974509d4c2283e2f0107d3c58344d0258f
Joseph T. Ball <jball321@live.com>
2018-04-24 02:04:23 -0400

    Finished mobile styling for wedding party component and added code to toggle bridesmaid and groomsmen cards.

--- client/app/pages/Gallery.js
+++ client/app/pages/Gallery.js
@@ -2,11 +2,9 @@ import React from 'react';
 
 const Gallery = props => {
     return (
-        <main>
-            <section className="content">
-                <h2>Gallery</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu tristique metus, at maximus nisl. Aliquam in neque rhoncus, suscipit sem eget, sodales neque. Suspendisse sit amet ipsum leo. Phasellus a nisl viverra, molestie neque et, aliquam mauris. Nullam vulputate augue a massa accumsan blandit.</p>
-                <button className="rsvp" disabled>RSVP</button>
+        <main className="gallery">
+            <section className="content gallery-content">
+                <h2>Sorry! This page is still under development.</h2>
             </section>
         </main>
     )
--- client/app/pages/WeddingParty.js
+++ client/app/pages/WeddingParty.js
@@ -1,43 +1,131 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const WeddingParty = props => {
-    return (
-        <main>
-            <section className="content weddingParty-content">
-                <h2><span className="selected">Bridesmaids</span> / <span className="">Groomsmen</span></h2>
-                <div className="bridesmaids photo-cards">
-                    <div className="card bridemaid-1">
-                        {/* <img className="portrait" src="" alt="portrait" /> */}
-                        <div className="label">
-                            <h3 className="first-name">FirstName</h3>
-                            <h3 className="last-name">LastName</h3>
+
+export default class WeddingParty extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            bmaidsSelect: 'selected',
+            gmenSelect: '',
+            bmaidsHide: '',
+            gmenHide: 'hide'
+        };
+
+        this.showBridesmaids = this.showBridesmaids.bind(this);
+        this.showGroomsmen = this.showGroomsmen.bind(this);
+    }
+
+    showBridesmaids() {
+        if (this.state.bmaidsSelect !== 'selected')  {
+            this.setState({
+                bmaidsSelect: 'selected',
+                gmenSelect: '',
+                bmaidsHide: '',
+                gmenHide: 'hide'
+            });
+        }
+    }
+
+    showGroomsmen() {
+        if (this.state.gmenSelect !== 'selected')  {
+            this.setState({
+                bmaidsSelect: '',
+                gmenSelect: 'selected',
+                bmaidsHide: 'hide',
+                gmenHide: ''
+            });
+        }
+    }
+
+    render() {
+        return (
+            <main>
+                <section className="content weddingParty-content">
+                    <h2><span onClick={this.showBridesmaids} className={this.state.bmaidsSelect}>Bridesmaids</span> / <span onClick={this.showGroomsmen} className={this.state.gmenSelect}>Groomsmen</span></h2>
+    
+                    <div className={`photo-cards ${this.state.bmaidsHide}`}>
+                        <div className="card bridemaid-1">
+                            <div className="label">
+                                <h3 className="first-name">FirstName</h3>
+                                <h3 className="last-name">LastName</h3>
+                                <h5>Maid of Honor</h5>
+                            </div>
+                        </div>
+                        <div className="card bridemaid-2">
+                            <div className="label">
+                                <h3 className="first-name">FirstName</h3>
+                                <h3 className="last-name">LastName</h3>
+                            </div>
+                        </div>
+                        <div className="card bridemaid-3">
+                            <div className="label">
+                                <h3 className="first-name">FirstName</h3>
+                                <h3 className="last-name">LastName</h3>
+                            </div>
+                        </div>
+                        <div className="card bridemaid-4">
+                            <div className="label">
+                                <h3 className="first-name">FirstName</h3>
+                                <h3 className="last-name">LastName</h3>
+                            </div>
+                        </div>
+                        <div className="card bridemaid-default">
+                            <div className="label">
+                                <h3 className="first-name">FirstName</h3>
+                                <h3 className="last-name">LastName</h3>
+                            </div>
+                        </div>
+                        <div className="card bridemaid-default">
+                            <div className="label">
+                                <h3 className="first-name">FirstName</h3>
+                                <h3 className="last-name">LastName</h3>
+                            </div>
                         </div>
                     </div>
-                    <div className="card bridemaid-2">
-                        {/* <img className="portrait" src="" alt="portrait" /> */}
-                        <div className="label">
-                            <h3 className="first-name">FirstName</h3>
-                            <h3 className="last-name">LastName</h3>
+    
+                    <div className={`photo-cards ${this.state.gmenHide}`}>
+                        <div className="card groomsman-default">
+                            <div className="label">
+                                <h3 className="first-name">FirstName</h3>
+                                <h3 className="last-name">LastName</h3>
+                                <h5>Best Man</h5>
+                            </div>
+                        </div>
+                        <div className="card groomsman-default">
+                            <div className="label">
+                                <h3 className="first-name">FirstName</h3>
+                                <h3 className="last-name">LastName</h3>
+                            </div>
+                        </div>
+                        <div className="card groomsman-default">
+                            <div className="label">
+                                <h3 className="first-name">FirstName</h3>
+                                <h3 className="last-name">LastName</h3>
+                            </div>
+                        </div>
+                        <div className="card groomsman-default">
+                            <div className="label">
+                                <h3 className="first-name">FirstName</h3>
+                                <h3 className="last-name">LastName</h3>
+                            </div>
+                        </div>
+                        <div className="card groomsman-default">
+                            <div className="label">
+                                <h3 className="first-name">FirstName</h3>
+                                <h3 className="last-name">LastName</h3>
+                            </div>
+                        </div>
+                        <div className="card groomsman-default">
+                            <div className="label">
+                                <h3 className="first-name">FirstName</h3>
+                                <h3 className="last-name">LastName</h3>
+                            </div>
                         </div>
                     </div>
-                    <div className="card bridemaid-3">
-                        {/* <img className="portrait" src="" alt="portrait" /> */}
-                        <div className="label">
-                            <h3 className="first-name">FirstName</h3>
-                            <h3 className="last-name">LastName</h3>
-                        </div>
-                    </div>
-                    <div className="card bridemaid-4">
-                        {/* <img className="portrait" src="" alt="portrait" /> */}
-                        <div className="label">
-                            <h3 className="first-name">FirstName</h3>
-                            <h3 className="last-name">LastName</h3>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </main>
-    )
+    
+                </section>
+            </main>
+        )
+    }
 }
-
-export default WeddingParty;
