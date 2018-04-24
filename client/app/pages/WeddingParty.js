60db9edc1b845c026090e3030cee024b9e0fa064
Joseph T. Ball <jball321@live.com>
2018-04-24 00:00:27 -0400

    Added stock bridesmaids photos and started styling wedding party component.

--- client/app/pages/WeddingParty.js
+++ client/app/pages/WeddingParty.js
@@ -3,18 +3,32 @@ import React from 'react';
 const WeddingParty = props => {
     return (
         <main>
-            <section className="content">
+            <section className="content weddingParty-content">
                 <h2><span className="selected">Bridesmaids</span> / <span className="">Groomsmen</span></h2>
-                <div className="photo-cards">
-                    <div className="card">
-                        <img className="portrait" src="" alt="portrait" />
+                <div className="bridesmaids photo-cards">
+                    <div className="card bridemaid-1">
+                        {/* <img className="portrait" src="" alt="portrait" /> */}
                         <div className="label">
                             <h3 className="first-name">FirstName</h3>
                             <h3 className="last-name">LastName</h3>
                         </div>
                     </div>
-                    <div className="card">
-                        <img className="portrait" src="" alt="portrait" />
+                    <div className="card bridemaid-2">
+                        {/* <img className="portrait" src="" alt="portrait" /> */}
+                        <div className="label">
+                            <h3 className="first-name">FirstName</h3>
+                            <h3 className="last-name">LastName</h3>
+                        </div>
+                    </div>
+                    <div className="card bridemaid-3">
+                        {/* <img className="portrait" src="" alt="portrait" /> */}
+                        <div className="label">
+                            <h3 className="first-name">FirstName</h3>
+                            <h3 className="last-name">LastName</h3>
+                        </div>
+                    </div>
+                    <div className="card bridemaid-4">
+                        {/* <img className="portrait" src="" alt="portrait" /> */}
                         <div className="label">
                             <h3 className="first-name">FirstName</h3>
                             <h3 className="last-name">LastName</h3>
